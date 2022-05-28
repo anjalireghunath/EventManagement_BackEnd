1bd3f6467ec49a194294a4c687357f79bd0cd633
anjalireghunath <anjalireghu98@gmail.com>
2022-05-28 20:08:43 +0530

    search and delete

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ const Mongoose=require("mongoose")
 var app=Express()
 app.use(Bodyparser.urlencoded({extended:true}))
 app.use(Bodyparser.json())
+app.use((req, res, next) => { 
+    res.setHeader("Access-Control-Allow-Origin", "*");  
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"   ); 
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"   ); 
+    next(); });
 
 var eventModel=Mongoose.model("event",
 new Mongoose.Schema(
@@ -28,20 +33,59 @@ app.post("/api/eventadd",(req,res)=>{
     var getVenue=req.body.venue
     var getOrganiser=req.body.organiser
     var getContact=req.body.contact
-    datas={"name":getName,"date":getDate,"venue":getVenue,"organiser":getOrganiser,"contact":getContact}
-    let myevent=new eventModel(datas)
-    myevent.save((error,datas)=>{
+    data={"name":getName,"date":getDate,"venue":getVenue,"organiser":getOrganiser,"contact":getContact}
+    let myevent=new eventModel(data)
+    myevent.save((error,data)=>{
         if(error){
-            res.send({"status":"error","datas":error})
+            res.send({"status":"error","data":error})
         }
         else{
-            res.send({"status":"success","datas":datas})
+            res.send({"status":"success","data":data})
         }
     })
 })
 
-app.post("/api/eventview",(req,res)=>{
-    res.send("hai")
+app.get("/api/eventview",(req,res)=>{
+    eventModel.find(
+        (error,data)=>{
+            if(error)
+            {
+                res.send({"status":"error"})
+            }
+            else
+            {
+                res.send(data)
+            }
+        }
+    )
+})
+
+app.post("/api/search",(req,res)=>{
+    var getDate=req.body
+    eventModel.find(getDate,(error,data)=>{
+        if(error)
+        {
+            res.send({"status":"error"})
+        }
+        else
+        {
+            res.send(data)
+        }
+    })
+})
+
+app.post("/api/delete",(req,res)=>{
+    var getId=req.body
+    eventModel.findByIdAndRemove(getId,(error,data)=>{
+        if(error)
+        {
+res.send({"status":"error"})
+        }
+        else
+        {
+res.send({"status":"success"})
+        }
+    })
 })
 
 app.listen(4007,()=>{
